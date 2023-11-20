52d3939bc3a09f44dd1f18d2ce71bbd482397eaf
NosukeSinjo <ayataka3270505@gmail.com>
2023-11-21 05:25:33 +0900

    add_products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+    def new
+        @product = Product.new
+    end
+    def create
+        image = params[:product][:image].read
+        @product = Product.new(title: params[:product][:title], price: params[:product][:price], description: params[:product][:description], image: image)
+        @product.save
+        redirect_to '/products'
+    end
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        redirect_to products_path
+    end
+    def show
+        @product = Product.find(params[:id])
+    end
+    def get_image
+        product = Product.find(params[:id])
+        send_data product.image, disposition: :inline, type: 'product/png'
+    end     
+end
