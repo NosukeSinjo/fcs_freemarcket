0640ed175d4168bd39b90f26a6b4598d635ffc42
NosukeSinjo <ayataka3270505@gmail.com>
2023-11-21 05:24:06 +0900

    delete merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,9 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_merchant, :signed_in?
-
-  private
-
-  def sign_in(merchant)
-    session[:merchant_id] = merchant.id
-  end
-
-  def sign_out
-    session[:merchant_id] = nil
-  end
-
-  def current_merchant
-    @current_merchant ||= Merchant.find_by(id: session[:merchant_id]) if session[:merchant_id]
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def signed_in?
-    return if current_merchant
-
-    redirect_to login_path
-  end
+    private
+        def current_user
+            if session[:name]
+                User.find_by(name: session[:name])
+            end
+        end
+        helper_method :current_user
 end
