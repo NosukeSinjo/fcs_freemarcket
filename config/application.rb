d4133522c177d88474ed2bf4d4b6df9c554dee0d
NosukeSinjo <ayataka3270505@gmail.com>
2023-11-21 05:22:52 +0900

    add_slim

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,13 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+module App
+  class Application < Rails::Application
+    config.generators.template_engine = :slim
+  end
+end
 
-module QuattroTapyrusAcquirer
+module FcsFreeMarcket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "quattro_tapyrus_acquirer_production"
+  # config.active_job.queue_name_prefix = "fcs_freemarcket_production"
 
   config.action_mailer.perform_caching = false
 
