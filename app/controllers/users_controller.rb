e4890bc0a46e9358486b403989d0ccf01344b341
NosukeSinjo <ayataka3270505@gmail.com>
2023-11-21 05:26:29 +0900

    fix_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-    def new; end
-  
-    def show
-      @user = current_user
+    
+    def new
+        @user = User.new
     end
     
     def create
-      @user = User.new(user_params)
-      if @user.save!
-        flash[:notice] = "登録が完了しました"
-        redirect_to root_path
-      else
-        render 'new'
-      end
-  
-      sign_in(@user)
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "登録が完了しました"
+            redirect_to top_login_path
+        else
+            render new_user_path
+        end
     end
-  
+    
+    def show
+        @user = current_user
+    end
+
     private
     def user_params
-      params.require(:user).permit(:user_name)
+        params.require(:user).permit(:name)
     end
-  end
-  
+end
