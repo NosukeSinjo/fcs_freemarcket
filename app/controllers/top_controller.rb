de0ebeab447215b7116e3019d0c7113f885995b5
NosukeSinjo <ayataka3270505@gmail.com>
2023-12-18 20:49:15 +0900

    update top_controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,24 +1,35 @@
 class TopController < ApplicationController
     def main
-        if session[:id]
-            render "products"
-        else
-            render "login"
-        end
+        render "login"
     end
     
     def login
         user = User.find_by(user_name: params[:user_name])
         if user
             session[:name] = user.user_name
-            redirect_to products_path
+            redirect_to merchants_path
         else
             render "login"
         end
     end
-    
+
+    def shoplogin
+        merchant = Merchant.find_by(merchant_name: params[:merchant_name])
+        if merchant
+            session[:name] = merchant.merchant_name
+            redirect_to merchant_path(merchant.id)
+        else
+            render "shoplogin"
+        end
+    end
+
     def logout
-        session.delete(:id)
+        reset_session
         redirect_to top_main_path
-    end 
+    end
+
+    def shoplogout
+        session.delete(:merchant_name)
+        redirect_to top_main_path
+    end
 end
