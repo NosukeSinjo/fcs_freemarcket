1ae65d4e698814e1c106dd7cb16bcc1ff54d6192
NosukeSinjo <ayataka3270505@gmail.com>
2023-12-18 20:48:22 +0900

    update application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
 class ApplicationController < ActionController::Base
+        helper_method :current_user, :current_merchant, :merchant_logged_in?
+
     private
-        def current_user
-            if session[:user_name]
-                User.find_by(user_name: session[:user_name])
-            end
-        end
-        helper_method :current_user
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def sign_in(merchant)
+        session[:merchant_id] = merchant.id
+    end
+
+    def sign_in(user)
+        session[:user_id] = user.id
+    end
+
+    def merchant_logged_in?
+        !current_merchant.nil?
+    end
+
+    def sign_out
+        session[:id] = nil
+    end
+
+    def current_merchant
+        @current_merchant ||= Merchant.find_by(id: session[:merchant_id]) if session[:merchant_id]
+    end
 end
