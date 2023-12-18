2305903a596926d0d57121661e189ca227550170
NosukeSinjo <ayataka3270505@gmail.com>
2023-12-18 20:47:28 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   post 'top/login'
 
-  get 'top/logout'
-  get 'top/login'
+  get 'signup', to: 'merchants#new'
+  post 'signup', to: 'merchants#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 
+
+  resources :merchants
   resources :users
-
+  resources :products
+  
   root 'top#main'
   get 'top/main'
-  post 'products/index'
+  
   get 'get_image/:id', to: 'products#get_image'
-  resources :products
+  
   resources :orders
 end
