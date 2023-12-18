17d1064be5d6fed9a5903996aeec13e6be47a8f6
NosukeSinjo <ayataka3270505@gmail.com>
2023-12-18 20:48:06 +0900

    update products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,26 +2,38 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
     end
+
     def new
-        @product = Product.new
+        @product = ????.merchant.products.new
     end
+
     def create
-        image = params[:product][:image].read
-        @product = Product.new(title: params[:product][:title], price: params[:product][:price], description: params[:product][:description], image: image)
-        @product.save
-        redirect_to '/products'
+        @product = ????.products.new(product_params)
+        if @product.save!
+            flash[:success] = "商品を追加しました。"
+            redirect_to merchant_path(@merchant.id)
+        end
     end
+
     def destroy
         product = Product.find(params[:id])
         product.destroy
         redirect_to products_path
     end
+
     def show
         @product = Product.find(params[:id])
     end
+
     def get_image
         product = Product.find(params[:id])
         send_data product.image, disposition: :inline, type: 'product/png'
-    end     
+    end
+
+    private
+    def product_params
+        image = params[:product][:image].read
+        params.require(:product).permit(:merchant_id, :title, :price, :description, :image)
+    end
 end
 
